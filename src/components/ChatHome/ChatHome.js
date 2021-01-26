25597b55061a55164ad89c6bf658590a2bb68a76
Peridax <evandrojr902@gmail.com>
2021-01-26 16:23:48 -0500

    Created chat functionality
    - Created chat component
    - Created chat form
    - Set up the link to chat in the header
    - Set up the socket event handler
    - Tested chat functionality (working)

--- src/components/ChatHome/ChatHome.js
+++ src/components/ChatHome/ChatHome.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom'
+import { v4 as uuid } from 'uuid'
+
+// Import react bootstrap
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
 import apiUrl from '../../apiConfig'
 
@@ -7,22 +12,81 @@ import apiUrl from '../../apiConfig'
 import socketio from 'socket.io-client'
 const endpoint = apiUrl
 
+let io = null
+
 class ChatHome extends Component {
   constructor (props) {
     super(props)
-    this.state = {}
+    this.state = {
+      messages: [],
+      chat_message: ''
+    }
   }
 
   componentDidMount () {
-    const io = socketio(endpoint)
+    io = socketio(endpoint)
 
-    io.on('ping', () => {
-      console.log('pong')
+    io.on('newMessage', message => {
+      this.setState(prevState => {
+        return {
+          messages: [ ...prevState.messages, { id: uuid(), content: message } ]
+        }
+      })
     })
   }
 
+  // Create a controlled input
+  handleChange = event => this.setState({
+    [event.target.name]: event.target.value
+  })
+  // handleChange = event => {
+  //   event.persist()
+  //
+  //   this.setState(prev => {
+  //     return {
+  //       chat_message: prev.chat_message + event.target.value
+  //     }
+  //   })
+  // }
+
+  handleMessage = event => {
+    event.preventDefault()
+
+    Promise.resolve()
+      .then(io.emit('message', this.state.chat_message))
+      .then(this.setState({ chat_message: '' }))
+      .catch(console.error)
+  }
+
   render () {
-    return <p>Chat</p>
+    const messageJsx = this.state.messages.map(message => (
+      <div key={message.id} className="message">{message.content}</div>
+    ))
+
+    return (
+      <div className="row">
+        <div className="col-12">
+          {this.state.messages ? messageJsx : <p>No messages</p>}
+        </div>
+        <div className="col-12">
+          <Form onSubmit={this.handleMessage}>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Message</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Write your message"
+                name="chat_message"
+                value={this.state.chat_message}
+                onChange={this.handleChange}
+              />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Send Message
+            </Button>
+          </Form>
+        </div>
+      </div>
+    )
   }
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,6 +4,7 @@ import Navbar from 'react-bootstrap/Navbar'
 
 const authenticatedOptions = (
   <Fragment>
+    <Nav.Link href="#chat">Chat</Nav.Link>
     <Nav.Link href="#change-password">Change Password</Nav.Link>
     <Nav.Link href="#sign-out">Sign Out</Nav.Link>
   </Fragment>
