00300c40253c580eb5ae5eec83144fd3391f8dad
RadarRadarRadar <mharold214@gmail.com>
2021-01-28 13:29:09 -0600

    Remove InputGroup require

--- src/components/ChatHome/ChatHome.js
+++ src/components/ChatHome/ChatHome.js
@@ -5,7 +5,6 @@ import { v4 as uuid } from 'uuid'
 // Import react bootstrap
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
-import InputGroup from 'react-bootstrap/InputGroup'
 // import Row from 'react-bootstrap/Row'
 // import Col from 'react-bootstrap/Col'
 
