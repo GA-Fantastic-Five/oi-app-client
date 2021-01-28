cc4fd54988548166bb95f28427d3ad215f121a42
RadarRadarRadar <mharold214@gmail.com>
2021-01-28 14:09:57 -0600

    Create routes for axios requests to api - Created axios requests for the server api.

---
+++ src/api/profiles.js
@@ -0,0 +1,54 @@
+import apiUrl from '../apiConfig'
+import axios from 'axios'
+
+export const createProfile = (profile, user) => {
+  return axios({
+    url: apiUrl + '/profiles',
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${user.token}`
+    },
+    data: {
+      profile: {
+        nickname: profile.nickname,
+        avatar: profile.avatar
+      }
+    }
+  })
+}
+
+export const indexProfiles = (user) => {
+  return axios({
+    url: apiUrl + '/profiles',
+    method: 'GET',
+    headers: {
+      'Authorization': `Bearer ${user.token}`
+    }
+  })
+}
+
+export const showProfile = (profile, user) => {
+  return axios({
+    url: apiUrl + '/profiles/' + profile.nickname,
+    method: 'GET',
+    headers: {
+      'Authorization': `Bearer ${user.token}`
+    }
+  })
+}
+
+export const updateProfile = (profile, user) => {
+  return axios({
+    url: apiUrl + '/profile',
+    method: 'PATCH',
+    headers: {
+      'Authorization': `Bearer ${user.token}`
+    },
+    data: {
+      profile: {
+        nickname: profile.nickname,
+        avatar: profile.avatar
+      }
+    }
+  })
+}
